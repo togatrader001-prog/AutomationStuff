fcb2dfd4449ef1792a8ee8775087f4e8871e71ae
togatrader001-prog <TogaTrader001@gmail.com>
2026-09-07 23:02:49 -0400

    second test being added

--- tests/UserRegistrationAndDeletion.spec.js
+++ tests/UserRegistrationAndDeletion.spec.js
@@ -1,7 +1,7 @@
 // @ts-check
 import { test, expect } from "@playwright/test";
 
-test("sample test run", async ({ page }) => {
+test("E2E - User Registration and Deletion", async ({ page }) => {
 	const testEmail = `smellyjapanesegirl${Date.now()}@gmail.com`;
 
 	await page.goto("https://automationexercise.com/");
